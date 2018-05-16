8c17e4ce439f464a2415ed503a37f6c8d824776f
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-16 21:58:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,43 +26,12 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-app.use('/peerjs', require('peer').ExpressPeerServer(app.listen(server_port)));
+//app.use('/peerjs', require('peer').ExpressPeerServer(app.listen(server_port)));
 /**
  *  Show in the console the URL access for other devices in the network
  */
-Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
 
-    ifaces[ifname].forEach(function (iface) {
-        if ('IPv4' !== iface.family || iface.internal !== false) {
-            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-            return;
-        }
-        
-        console.log("");
-        console.log("Welcome to the Chat Sandbox");
-        console.log("");
-        console.log("Test the chat interface from this device at : ", "https://localhost:8443");
-        console.log("");
-        console.log("And access the chat sandbox from another device through LAN using any of the IPS:");
-        console.log("Important: Node.js needs to accept inbound connections through the Host Firewall");
-        console.log("");
-
-        if (alias >= 1) {
-            console.log("Multiple ipv4 addreses were found ... ");
-            // this single interface has multiple ipv4 addresses
-            console.log(ifname + ':' + alias, "https://"+ iface.address + ":8443");
-        } else {
-            // this interface has only one ipv4 adress
-            console.log(ifname, "https://"+ iface.address + ":8443");
-        }
-
-        ++alias;
-    });
-});
-
-// Allow access from all the devices of the network (as long as connections are allowed by the firewall)
-var LANAccess = "0.0.0.0";
+// Allow access from all the devices of the network (as long as connections are allowed by the firewall)\
 // For http
 httpServer.listen(server_port, server_ip_address);
 // For https
