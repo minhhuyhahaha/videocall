d734a9f9bad395925ea713403688ff0452a62b3e
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-16 22:06:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,20 +22,58 @@ var app = express();
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
-app.use('/peerjs', require('peer').ExpressPeerServer(app.listen(server_port)));
+var PeerServer = require('peer').PeerServer;
+
+var server = PeerServer({
+    port: 9000,
+    path: '/peerjs',
+    ssl: {
+        key: fs.readFileSync('./certificates/key.pem', 'utf8'),
+        cert: fs.readFileSync('./certificates/cert.pem', 'utf8')
+    }
+});
 /**
  *  Show in the console the URL access for other devices in the network
  */
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
 
-// Allow access from all the devices of the network (as long as connections are allowed by the firewall)\
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+        
+        console.log("");
+        console.log("Welcome to the Chat Sandbox");
+        console.log("");
+        console.log("Test the chat interface from this device at : ", "https://localhost:8443");
+        console.log("");
+        console.log("And access the chat sandbox from another device through LAN using any of the IPS:");
+        console.log("Important: Node.js needs to accept inbound connections through the Host Firewall");
+        console.log("");
+
+        if (alias >= 1) {
+            console.log("Multiple ipv4 addreses were found ... ");
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, "https://"+ iface.address + ":8443");
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, "https://"+ iface.address + ":8443");
+        }
+
+        ++alias;
+    });
+});
+
+// Allow access from all the devices of the network (as long as connections are allowed by the firewall)
+var LANAccess = "0.0.0.0";
 // For http
-httpServer.listen(server_port, server_ip_address);
+httpServer.listen(8080, LANAccess);
 // For https
-//httpsServer.listen(server_port, LANAccess);
+httpsServer.listen(8443, LANAccess);
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
