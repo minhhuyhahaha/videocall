04eb1b9eb48274f84785e428e1704a4b66d670e4
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-16 21:59:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ var app = express();
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
-//app.use('/peerjs', require('peer').ExpressPeerServer(app.listen(server_port)));
+app.use('/peerjs', require('peer').ExpressPeerServer(app.listen(server_port)));
 /**
  *  Show in the console the URL access for other devices in the network
  */
