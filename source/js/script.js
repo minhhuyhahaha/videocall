8ba12cabdcbe495e451992b24666ff79ebc4b900
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-17 23:55:56 +0700

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,25 @@ var peer_name;
 var username;
 var conn;
 var connected = false;
-var peer = new Peer({key: 'peerjs', host: 'peerjs-stc.herokuapp.com', secure: true, port: 443});
+var customConfig;
+$(document).ready( function () {
+    $.ajax ({
+        url: "https://global.xirsys.net/_turn/myturnserver/",
+        type: "PUT",
+        async: false,
+        headers: {
+            "Authorization": "Basic " + btoa("songtinhcam03:a7f4da8c-59ef-11e8-826f-09fc0001476b")
+        },
+        success: function (res){
+            customConfig = res.v.iceServers;
+        }
+    });
+});
+var peer = new Peer({key: 'peerjs',
+                    host: 'peerjs-stc.herokuapp.com',
+                    secure: true,
+                    port: 443,
+                    config: customConfig});
 var socket = io();
 socket.on('join', function (info) {
     var x = document.getElementById("list-online");
