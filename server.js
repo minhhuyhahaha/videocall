a0bf5413ffa61a78d684d121c38d36e41d92751a
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-18 12:06:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-/**
- * This script starts a https server accessible at https://localhost:8443
- * to test the chat
- *
- * @author Carlos Delgado
- */
-var http   = require('http');
-var path   = require("path");
+var http = require('http');
+var path = require("path");
 var express = require('express');
-
 var app = express();
 var httpServer = http.createServer(app);
 var io = require("socket.io")(httpServer);
-//var LANAccess = "0.0.0.0";
 var port = process.env.PORT || 8080;
+const listUser = [];
 httpServer.listen(port);
 
-io.on('connection', function (user) {
-    user.on('join', function (info) {
-        user.broadcast.emit('join',info);
-    })
+io.on('connection', (socket) => {
+    socket.on('new_user', (user) => {
+    	var isExist = listUser.some(i => i.username === user.username);
+    	if(isExist){
+    		return socket.emit('register_fail');
+    	}
+    	socket.peerid = user.peerid;
+    	listUser.push(user);
+    	socket.emit("list_online", listUser);
+        socket.broadcast.emit('new_user',user);
+    });
+
+    socket.on('disconnect', () => {
+    	var index = listUser.findIndex(i => i.peerid === socket.peerid);
+    	listUser.splice(index, 1);
+    	socket.broadcast.emit('user_disconnect',socket.peerid);
+    });
 });
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-// Expose the css and js resources as "resources"
 app.use('/resources', express.static('./source'));
