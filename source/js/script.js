bb6b855707c9f250858bd9af09b0138b665449cf
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-05-18 14:45:34 +0700

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -3,33 +3,24 @@ var peer_name;
 var username;
 var conn;
 var connected = false;
-//var customConfig;
-// $.ajax ({
-//     url: "https://global.xirsys.net/_turn/myturnserver/",
-//     type: "PUT",
-//     async: false,
-//     headers: {
-//         "Authorization": "Basic " + btoa("songtinhcam03:a7f4da8c-59ef-11e8-826f-09fc0001476b")
-//     },
-//     success: function (res){
-//         customConfig = res.v.iceServers;
-//     }
-// });
+var customConfig;
+$.ajax ({
+    url: "https://global.xirsys.net/_turn/myturnserver/",
+    type: "PUT",
+    async: false,
+    headers: {
+        "Authorization": "Basic " + btoa("songtinhcam03:a7f4da8c-59ef-11e8-826f-09fc0001476b")
+    },
+    success: function (res){
+        customConfig = res.v.iceServers;
+    }
+});
 var peer = new Peer({key: 'peerjs',
                     host: 'peerjs-stc.herokuapp.com',
                     secure: true,
                     port: 443,
-                    config: {
-                        'iceServers': [
-                                        { url: 'stun:stun1.l.google.com:19302' },
-                                        {
-                                            url: 'turn:numb.viagenie.ca',
-                                            credential: 'muazkh',
-                                            username: 'webrtc@live.com'
-                                        }
-                                      ]
-                    }
-            });
+                    config: {'iceServers': customConfig }
+                    });
 var socket = io();
 
 socket.on('new_user', function (user) {
